d3db5fd37271697d589a72b3a64a950d02b3504a
KUCA Brice <kuca.b@sfeir.com>
2024-10-09 11:20:15 +0200

    add book api

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
     const [name, setName] = useState('Vite')
     const [count, setCount] = useState(0)
-    const books = [
-        {
-            id:1,
-            name: "livre1",
-        },
-        {
-            id:2,
-            name: "livre2",
-        },
-        {
-            id:3,
-            name: "livre3",
-        },
-    ]
+    const [books, setBooks] = useState([])
+    useEffect(() => {
+        fetch("https://express-mongodb-k76a.onrender.com/api/books")
+            .then(res => res.json())
+            .then(data => setBooks(data))
+            .catch(error => console.error('Error fetching books:', error))
+    }, [])
 
+    fetch("https://express-mongodb-k76a.onrender.com/api/books").then(res => {
+        console.log(res.json())
+    })
     const numbers = Array.from({ length: 100 }, (_, i) => i + 1)
 
     return(
@@ -28,8 +22,8 @@ function App() {
             <h1>Hello world {name}</h1>
             <p> Counter : {count}</p>
             <button onClick={() => setName("toto")}>Change name</button>
-            <button onClick={() => setCount((count) => count + 1)}>augmente count</button>
-            <button onClick={() => setCount((count) => count - 1)}>réduis count</button>
+            <button onClick={() => setCount((count) => count + 1)}>increment count</button>
+            <button onClick={() => setCount((count) => count - 1)}>decrement count</button>
             <ul>
                 {books.map(book => {
                     return <li key={book.id}>{book.name}</li>
